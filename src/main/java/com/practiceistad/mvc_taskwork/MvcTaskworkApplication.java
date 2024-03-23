2f400c223ee164335d131130634cb6d9e0272583
thathyda <thydatha484@gmail.com>
2024-03-23 23:54:08 +0700

    start trend

---
+++ src/main/java/com/practiceistad/mvc_taskwork/MvcTaskworkApplication.java
@@ -0,0 +1,13 @@
+package com.practiceistad.mvc_taskwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcTaskworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcTaskworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practiceistad/mvc_taskwork/MvcTaskworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practiceistad.mvc_taskwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcTaskworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
